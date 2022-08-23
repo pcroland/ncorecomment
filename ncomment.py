63d8e561c0cdd8ce6e1b67686ce4b7cadc63b1f0
pcroland <pcroland@protonmail.ch>
2022-08-23 10:49:14 +0200

    # ncomment 1.5.0: - colored argparser - removed `-2fa` option - added `twofactor` option to login.toml   login will ask for the code if it's set to true

--- ncomment.py
+++ ncomment.py
@@ -14,6 +14,9 @@ import toml
 from bs4 import BeautifulSoup as soup
 from rich import print as rprint
 
+prog_name = 'ncomment'
+prog_version = '1.5.0'
+
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 def opentoml(fl):
@@ -71,17 +74,17 @@ def hidden_check(id_):
         rprint(f'https://ncore.pro/t/{id_} | [not italic bold yellow]{name[:truncate() - length]}[/not italic bold yellow]')
 
 
-def login(username, password):
-    rprint(f'logging in as [bold cyan]{username}[/bold cyan]', end='', flush=True)
-    if args.twofactor != '':
-        rprint(f' with 2FA: [bold cyan]{args.twofactor}[/bold cyan][white]...[/white] ', end='', flush=True)
+def login(username, password, twofactor):
+    rprint(f'logging in as [bold cyan]{username}[/bold cyan][not bold white]...[/not bold white]')
+    if twofactor:
+        twofactor_code = input('2FA code: ')
     else:
-        print('... ', end='', flush=True)
+        twofactor_code = ''
     form_data = {
         'submitted': 1,
         'nev': username,
         'pass': password,
-        '2factor': args.twofactor,
+        '2factor': twofactor_code,
         'ne_leptessen_ki': 1
     }
     session.post('https://ncore.pro/login.php?2fa', form_data, allow_redirects=False)
@@ -93,37 +96,62 @@ def login(username, password):
         sys.exit(1)
 
 
-parser = argparse.ArgumentParser(add_help=False)
+class RParse(argparse.ArgumentParser):
+    def _print_message(self, message, file=None):
+        if message:
+            if message.startswith('usage'):
+                message = f'[bold cyan]{prog_name}[/bold cyan] {prog_version}\n\n{message}'
+                message = re.sub(r'(-[a-z]+\s*|\[)([A-Z]+)(?=]|,|\s\s|\s\.)', r'\1[{}]\2[/{}]'.format('bold color(231)', 'bold color(231)'), message)
+                message = re.sub(r'((-|--)[0-9]?[a-z]+)', r'[{}]\1[/{}]'.format('green', 'green'), message)
+                message = message.replace('usage', f'[yellow]USAGE[/yellow]')
+                message = message.replace('options', f'[yellow]FLAGS[/yellow]', 1)
+                message = message.replace(self.prog, f'[bold cyan]{self.prog}[/bold cyan]')
+            message = f'[not bold white]{message.strip()}[/not bold white]'
+            rprint(message)
+
+
+class CustomHelpFormatter(argparse.RawTextHelpFormatter):
+    def _format_action_invocation(self, action):
+        if not action.option_strings or action.nargs == 0:
+            return super()._format_action_invocation(action)
+        default = self._get_default_metavar_for_optional(action)
+        args_string = self._format_args(action, default)
+        return ', '.join(action.option_strings) + ' ' + args_string
+
+
+parser = RParse(
+    add_help=False,
+    formatter_class=lambda prog: CustomHelpFormatter(prog, width=80, max_help_position=33)
+)
+
 parser.add_argument('-h', '--help',
                     action='help',
                     default=argparse.SUPPRESS,
-                    help='Show this help message.')
+                    help='show help')
 parser.add_argument('-v', '--version',
                     action='version',
                     version='ncomment 1.4.2',
-                    help='Shows version.')
-parser.add_argument('-2fa', '--twofactor',
-                    default='',
-                    help='2FA code for login.')
+                    help='show version')
 parser.add_argument('-s', '--search',
-                    help='Search word.')
+                    help='search word')
 parser.add_argument('-d', '--date',
-                    help='Date for comment comparing.')
+                    help='date for comment comparing')
 parser.add_argument('-e', '--exact',
                     action='store_true',
-                    help='Only search in torrents that actually contains the search string in the torrent name.')
+                    help='only search in torrents that actually contain the search string in the name.')
 parser.add_argument('-a', '--all',
                     action='store_true',
-                    help='Search in every category, not just your own.')
+                    help='search in all, not just in your own categories')
 parser.add_argument('-c', '--category',
+                    metavar='CAT',
                     nargs='*',
-                    help='Add search category(ies).')
+                    help='add search category(ies)')
 parser.add_argument('-m', '--mode',
                     default='title',
-                    help='Search mode. (title / description / imdb / uploader)')
+                    help='search mode. ([bold color(231)]title[/bold color(231)] / [bold color(231)]description[/bold color(231)] / [bold color(231)]imdb[/bold color(231)] / [bold color(231)]uploader)[/bold color(231)]')
 parser.add_argument('-r', '--hidden',
                     action='store_true',
-                    help='List hidden torrents from your uploads. If you use this switch other switches will be ignored.')
+                    help='list hidden torrents from your uploads\nif you use this switch other switches will be ignored')
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -132,16 +160,16 @@ if __name__ == '__main__':
     else:
         SCRIPT_PATH = os.path.dirname(__file__)
 
-    userpass_fl = os.path.join(SCRIPT_PATH, 'login.toml')
-    if not os.path.exists(userpass_fl):
+    config_fl = os.path.join(SCRIPT_PATH, 'login.toml')
+    if not os.path.exists(config_fl):
         print(f'[bold red]ERROR: login.toml is missing.[/bold red]')
         sys.exit(1)
     else:
-        userpass = opentoml(userpass_fl)['login']
+        config = opentoml(config_fl)
 
-    username = userpass.split(':')[0]
-    password = userpass.split(':')[1]
-    twofactor = args.twofactor
+    username = config['user']
+    password = config['pass']
+    twofactor = config['twofactor']
 
     COOKIES_PATH = os.path.join(SCRIPT_PATH, 'cookies.txt')
     session = requests.Session()
@@ -149,13 +177,13 @@ if __name__ == '__main__':
 
     if not os.path.isfile(COOKIES_PATH):
         rprint('🍪.txt is missing, ', end='')
-        login(username, password)
+        login(username, password, twofactor)
     else:
         session.cookies.load()
         response = session.get('https://ncore.pro/')
         if 'login.php' in response.url:
             rprint('🍪.txt expired, ', end='')
-            login(username, password)
+            login(username, password, twofactor)
 
     LOG_PATH = os.path.join(SCRIPT_PATH, 'log.json')
     if not os.path.isfile(LOG_PATH):
